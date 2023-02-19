568153957cc43ee3ce4ba161b60b21eb7440e214
Vinh Chuc <vinh.chuc@gmail.com>
2023-02-19 20:47:49 +0000

    Add vsnip shortcuts

--- nvim_config/init.lua
+++ nvim_config/init.lua
@@ -109,6 +109,11 @@ cmp.setup.cmdline(':', {
     })
 })
 
+vim.cmd("imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'");
+vim.cmd("smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'");
+vim.cmd("imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'");
+vim.cmd("smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'");
+
 -----------------------
 -- Autocompletion END -
 -----------------------
