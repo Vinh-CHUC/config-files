ca187f9e94656cc9e7f04fcaaa03f5158452fc83
Vinh Chuc <vinh.chuc@skyral.io>
2024-02-21 20:39:50 +0000

    Add some abbrevations to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
     starship init fish | source
+
+    # Abbreviations
+    abbr gsf 'git st --porcelain | fzf'
+    abbr glf "git log --pretty=format:'%h %an %ad %s' --date=short | fzf"
+    abbr ll 'eza -l'
 end
