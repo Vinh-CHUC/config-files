93c8951a6fe1a31ad2dbbe0b3adf42193643780a
Vinh Chuc <vinh.chuc@skyral.io>
2024-02-22 11:25:42 +0000

    Add PATHs to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,4 +7,20 @@ if status is-interactive
     abbr gsf 'git st --porcelain | fzf'
     abbr glf "git log --pretty=format:'%h %an %ad %s' --date=short | fzf"
     abbr ll 'eza -l'
+
+    # Paths
+    set PATHS \
+        /usr/local/bin \
+        $HOME/bin \
+        $HOME/.local/bin \
+        $HOME/.cargo/bin \
+        $HOME/.ghcup/bin \
+        $HOME/.cabal/bin \
+        $HOME/go/bin
+    for p in $PATHS
+        test -d $p; and fish_add_path $p
+    end
+
+    set -x FZF_DEFAULT_COMMAND 'fd --type file --hidden --no-ignore'
+    set -x POETRY_VIRTUALENVS_IN_PROJECT 0
 end
