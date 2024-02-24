dbd63726786fe82f1bfc8a8acda4a48fb825436a
Vinh Chuc <vinh.chuc@skyral.io>
2024-02-24 11:12:18 +0000

    Startup configs and telescope grep tweaks

--- nvim_config/lua/my_telescope_config.lua
+++ nvim_config/lua/my_telescope_config.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<C-p>", ":Telescope find_files<cr>", {remap = false})
 vim.keymap.set("n", "<leader>fr", ":Telescope lsp_references<cr>", {remap = false})
-vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<cr>", {remap = false})
+vim.keymap.set("n", "<leader>g", ":Telescope live_grep<cr>", {remap = false})
 vim.keymap.set("n", "<leader>fb", ":Telescope buffers<cr>", {remap = false})
 vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<cr>", {remap = false})
