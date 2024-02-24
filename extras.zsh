2ea8fee8f3b931518e009bcfffddd7c23406ae26
Vinh Chuc <vinh.chuc@skyral.io>
2024-02-24 19:31:32 +0000

    Retire bits from extras.zsh

--- extras.zsh
+++ extras.zsh
@@ -1,19 +1,3 @@
-export FZF_DEFAULT_COMMAND='fd --type file --hidden --no-ignore'
 
-alias ll='eza -l'
 
-export PATH="$HOME/bin:$PATH"
-[ -f "$HOME/.cargo/env" ] && source "$HOME/.cargo/env"
-[ -f "$HOME/.ghcup/env" ] && source "$HOME/.ghcup/env" # ghcup-env
 
-## HomeBrew
-export PATH="/usr/local/bin:$PATH"
-
-## Poetry
-export PATH="$HOME/.local/bin:$PATH"
-fpath+=~/.zfunc
-autoload -Uz compinit && compinit
-export POETRY_VIRTUALENVS_IN_PROJECT=0
-
-## Go
-export PATH="$HOME/go/bin:$PATH"
