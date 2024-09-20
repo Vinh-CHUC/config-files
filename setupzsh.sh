b6ed8d9e32bc93de71d1c0f05e38e8b8140cf8a7
Vinh Chuc <vinh.chuc@skyral.io>
2024-09-20 23:05:42 +0100

    Cleanup

--- setupzsh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-ln -sf "$(pwd)/prezto" "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-      ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
