6756f90fab7dd7875d78a749e9801796e8e03154
Vinh Chuc <vinh.chuc@skyral.io>
2024-09-26 22:41:19 +0100

    Homebrew Linux

--- fish/config.fish
+++ fish/config.fish
@@ -31,4 +31,13 @@ if status is-interactive
     # Misc
     set -x FZF_DEFAULT_COMMAND 'fd --type file --hidden --no-ignore'
     set -x POETRY_VIRTUALENVS_IN_PROJECT 0
+
+    # Homebrew linux
+    set --global --export HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+    set --global --export HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+    set --global --export HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+    fish_add_path --global --move --path "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin";
+    if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
+    if not contains "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH; set --global --export INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH; end;
+    if test -n "$XDG_DATA_DIRS"; set --global --export XDG_DATA_DIRS "/home/linuxbrew/.linuxbrew/share" $XDG_DATA_DIRS; end;
 end
