0893578f7d9189fa09756cbe92a2126345817852
Vinh Chuc <vinh.chuc@gmail.com>
2025-06-28 22:59:59 +0100

    Fix slime python configuration

--- nvim_config/lua/my_slime_config.lua
+++ nvim_config/lua/my_slime_config.lua
@@ -1,6 +1,13 @@
 vim.g.slime_target = "zellij"
-vim.g.slime_python_ipython = 1
+vim.g.slime_python_ipython = 0
 vim.g.slime_haskell_ghci_add_let = 0
+vim.g.slime_default_config = {
+    session_id = "current",
+    relative_pane = "right",
+    relative_move_back = "left"
+}
+vim.g.slime_dont_ask_default = 1
+vim.g.slime_bracketed_paste = 1
 
 vim.keymap.set("x", "ff", "<Plug>SlimeRegionSend", {remap = false})
 vim.keymap.set("n", "ff", "<Plug>SlimeParagraphSend", {remap = false})
