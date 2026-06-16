f26cdae11275b23090f38ad34cb8074e157514b2
Vinh Chuc <vinh.chuc@gmail.com>
2026-06-16 22:14:52 +0100

    Fix opencode plugin following update

--- nvim_config/lua/plugins/init.lua
+++ nvim_config/lua/plugins/init.lua
@@ -99,24 +99,60 @@ return {
             },
           },
         },
-      },
-      config = function()
+    },
+    config = function()
         ---@type opencode.Opts
         vim.g.opencode_opts = {
+            -- Your configuration, if any; goto definition on the type or field for details
         }
 
-        vim.o.autoread = true -- Required for `opts.events.reload`
+        vim.o.autoread = true -- Required for `vim.g.opencode_opts.events.reload`
 
         -- Recommended/example keymaps
-        vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
-        vim.keymap.set({ "n", "x" }, "<leader>ox", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
-        vim.keymap.set({ "n", "t" }, "<leader>ot", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+        vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ") end, { desc = "Ask OpenCode…" })
+        vim.keymap.set({ "n", "x" }, "<leader>os", function() require("opencode").select() end,       { desc = "Select OpenCode…" })
 
-        vim.keymap.set({ "n", "x" }, "<leader>or",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
-        vim.keymap.set("n",          "<leader>ol", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
+        vim.keymap.set({ "n", "x" }, "<leader>or",  function() return require("opencode").operator("@this ") end,        { desc = "Append range to OpenCode", expr = true })
+        vim.keymap.set("n",          "<leader>ol", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Append line to OpenCode", expr = true })
 
-        vim.keymap.set("n", "<leader>osu", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
-        vim.keymap.set("n", "<leader>osd", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
+        local opencode_cmd = 'opencode --port'
+        ---@type snacks.terminal.Opts
+        local snacks_terminal_opts = {
+          win = {
+            position = 'right',
+            enter = false,
+          },
+        }
+
+        ---@type opencode.Opts
+        vim.g.opencode_opts = {
+          server = {
+            start = function()
+              require('snacks.terminal').open(opencode_cmd, snacks_terminal_opts)
+            end,
+          },
+        }
+
+        -- Can also leverage toggle functionality.
+        -- If you use <leader> here, remove 't' — otherwise Neovim will add input delay to your <leader> when typing in the terminal to watch for the mapping.
+        vim.keymap.set({ 'n', 't' }, '<leader>ot', function()
+          require('snacks.terminal').toggle(opencode_cmd, snacks_terminal_opts)
+        end, { desc = 'Toggle OpenCode' })
+
+        -- Optionally show upon submitting prompt
+        vim.api.nvim_create_autocmd('User', {
+          pattern = { 'OpencodeEvent:tui.command.execute' },
+          callback = function(args)
+            ---@type opencode.server.Event
+            local event = args.data.event
+            if event.properties.command == 'prompt.submit' then
+              local win = require('snacks.terminal').get(opencode_cmd, { create = false })
+              if win then
+                win:show()
+              end
+            end
+          end,
+        })
       end,
     },
 
